e9b7e3fe81921a98641f2de5977e6832d04d51a4
Michael Tran <michaelhtran23@gmail.com>
2025-02-23 02:05:29 +0000

    Update homework2.js

--- homework2.js
+++ homework2.js
@@ -6,6 +6,15 @@ Version: 1.0
 Description: Homework 2 JavaScript
 */
 
+/*
+    MISSO resources were used as a reference and guideline for this project.
+    All additional insights, modifications, and enhancements are my own work.
+    Extra explanations and additions have been included in the comments for clarity.
+    For full access to the MISSO resources, visit:
+    Homework 1: https://adhesive-tarragon-137.notion.site/Homework-1-15c348e9135280eea832f9de5dfbe8d9
+    Homework 2: https://adhesive-tarragon-137.notion.site/Homework-2-170348e9135280b4aca0e8794485b640
+*/
+
 //dynamic date js code
 const d= new Date();
 let text= d.toLocaleDateString();
