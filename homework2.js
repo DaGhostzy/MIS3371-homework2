e618bbc319d9fb9b8b952d8af7c346b764782f9a
Michael Tran <michaelhtran23@gmail.com>
2025-03-04 14:49:09 -0600

    Update homework2.js

--- homework2.js
+++ homework2.js
@@ -7,9 +7,11 @@ Description: Homework 2 JavaScript
 */
 
 /*
-    MISSO resources were used as a framework reference and guideline for this project.
+/*
+    The refrence structure and guidelines for this project were derived from the MISSO resources.
     All additional insights, modifications, and enhancements are my own work.
     Extra explanations and additions have been included in the comments for clarity.
+    
     For full access to the MISSO resources, visit:
     Homework 1: https://adhesive-tarragon-137.notion.site/Homework-1-15c348e9135280eea832f9de5dfbe8d9
     Homework 2: https://adhesive-tarragon-137.notion.site/Homework-2-170348e9135280b4aca0e8794485b640
@@ -25,7 +27,42 @@ let slider= document.getElementById("range");
 let output= document.getElementById("range-slider");
 output.innerHTML= slider.value;
 
-slider.onimput= function ()
+slider.oninput= function ()
 {
     output.innerHTML= this.value;
 }
+
+// validattion for date of birth
+function validateDob()
+ {
+   dob = document.getElementById("dob");
+   let date = new Date(dob.value);
+   let maxDate = new Date().setFullYear(new Date().getFullYear() - 120);
+
+  if (date > new Date()
+     {
+       document.getElementById("dob-error").innerHTML = 
+       "The date can NOT be in the future";
+       dob.value = "";
+       return false;
+     }
+  else if (date < new Date(maxDate))
+     {
+       document.getElementById("dob-error").innerHTML =
+       "The date can NOT be over 120 years ago";
+       dob.value = "";
+       return false;
+     }
+  else
+  {
+    document.getElementById("dob-error").innerHTML = "";
+    return true;
+  }
+}
+// structure guidelines derived from MISSO resources
+
+// validation for social security number
+function validateSsn()
+ {
+   const ssn = document.getElementById("ssn").value;
+  
